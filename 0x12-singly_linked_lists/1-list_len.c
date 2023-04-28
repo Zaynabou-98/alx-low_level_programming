8c692fe720c4cbec97cad79315db2d20673696b3
Zaynabou-98 <zineb.abouseir-etu@etu.univh2c.ma>
2023-04-28 15:21:46 -0700

    t

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,18 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @b: pointer to the list_t list
+ * @h: pointer to the list_t list
  *
  * Return: number of elements in b always 0 
  */
-size_t list_len(const list_t *b)
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
-	while (b)
+	while (h)
 	{
 		n++;
-		b = b->next;
+		h = h->next;
 	}
 	return (n);
 }
