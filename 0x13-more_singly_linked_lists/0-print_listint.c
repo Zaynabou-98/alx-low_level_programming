900042bd7b9266a95d42e5a384be237da5b9c1fb
Zaynabou-98 <zineb.abouseir-etu@etu.univh2c.ma>
2023-04-30 06:34:54 -0700

    t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,16 +5,16 @@
  * @h:linked list to printf
  *
  * Return: number of node
- * */
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t nm = 0;
+        size_t nm = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nm++;
-		}
-	return (nm);
-	}
+        while (h != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                nm++;
+                }
+        return (nm);
+        }
