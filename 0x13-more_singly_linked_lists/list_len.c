90e1792e09afd002585489808dc408a7b23aa693
Zaynabou-98 <zineb.abouseir-etu@etu.univh2c.ma>
2023-04-30 07:16:38 -0700

    t

---
+++ 0x13-more_singly_linked_lists/list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list to print
+ *
+ * Return: the numbers of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nm = 0;
+
+	while (h != NULL)
+	{
+		nm++;
+		h = h->next;
+	}
+
+	return (nm);
+}
