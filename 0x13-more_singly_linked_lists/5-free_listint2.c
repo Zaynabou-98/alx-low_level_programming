fb4969799d4e83623a39f81ba9a56e45d81e5180
Zaynabou-98 <zineb.abouseir-etu@etu.univh2c.ma>
2023-04-30 09:17:50 -0700

    t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
-	if (!head)
-		return(NULL);
+listint_t *current= *head;
+	if (head !== NULL)
+*current= *head		
+return;
 
-	while (*head)
+	while (*current != NULL)
 	{
+listint_t *temp = current;
+current= head
+head = temp		
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
